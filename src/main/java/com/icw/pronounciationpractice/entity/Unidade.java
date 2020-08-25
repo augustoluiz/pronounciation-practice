162bb43f0f427e24bfd64c59f84911aa7315668c
augustoluiz <augustogu7@yahoo.com.br>
2020-08-24 23:37:50 -0300

    Entidade Unidade

---
+++ src/main/java/com/icw/pronounciationpractice/entity/Unidade.java
@@ -0,0 +1,33 @@
+package com.icw.pronounciationpractice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Unidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @Size(max = 100)
+    @NotNull
+    @NotEmpty
+    private String nome;
+
+    @Column
+    @Size(max = 255)
+    private String descricao;
+}
