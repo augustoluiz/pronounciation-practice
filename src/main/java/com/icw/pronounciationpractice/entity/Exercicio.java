cf2362807e02cc9f4699000c99d19ab0619c1b79
augustoluiz <augustogu7@yahoo.com.br>
2020-08-24 23:56:25 -0300

    Entidade Exercicio + ajuste na Unidade

--- src/main/java/com/icw/pronounciationpractice/entity/Exercicio.java
+++ src/main/java/com/icw/pronounciationpractice/entity/Exercicio.java
@@ -1,37 +1,39 @@
 package com.icw.pronounciationpractice.entity;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Unidade {
+public class Exercicio {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
-    @Size(max = 100)
+    @Size(max = 50)
     @NotNull
     @NotEmpty
-    private String nome;
+    private String titulo;
 
     @Column
-    @Size(max = 255)
-    private String descricao;
+    @Min(0)
+    @NotNull
+    private int ordemApresentacao;
 
-    @OneToMany
-    private List<Exercicio> exercicios;
+    @ManyToOne
+    private Unidade unidade;
+
 }
