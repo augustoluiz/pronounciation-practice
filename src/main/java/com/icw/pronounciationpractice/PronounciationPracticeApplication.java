859f177a003d34c88b5b96e3ffe4b5bc42d703bd
augustoluiz <augustogu7@yahoo.com.br>
2020-08-24 23:29:06 -0300

    Commit inicial

---
+++ src/main/java/com/icw/pronounciationpractice/PronounciationPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.icw.pronounciationpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PronounciationPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PronounciationPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icw/pronounciationpractice/PronounciationPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icw.pronounciationpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PronounciationPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
